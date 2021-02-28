7dcf64e601cc6cf5bfc4a57b97af3c3f89e9c1af
victorhugo <victorblog410@gmail.com>
2021-02-28 01:47:48 -0300

    dependecy

--- pokemon_api/celery.py
+++ pokemon_api/celery.py
@@ -6,7 +6,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokemon_api.settings')
 
 app = Celery('pokemon_api')
 
-
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
